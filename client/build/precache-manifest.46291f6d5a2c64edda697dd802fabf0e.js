ed0cb45dfbf560a86f8ba5dd24ae148eb736bffb
stank2010 <stankkpp_hualaem@hotmail.com>
2020-10-26 20:41:26 +0700

    Hi

---
+++ client/build/precache-manifest.46291f6d5a2c64edda697dd802fabf0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a75976e82d150b663e1eb4345e2ebe8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea2118781ef5e8a09510",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6e070c6d93b139f0ad03",
+    "url": "/static/js/2.68633431.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/static/js/2.68633431.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea2118781ef5e8a09510",
+    "url": "/static/js/main.ab8326c2.chunk.js"
+  },
+  {
+    "revision": "90fbbed2e7fcd23e86b1",
+    "url": "/static/js/runtime-main.aedcd107.js"
+  }
+]);
